73145711b4749f6ae5f6f53692cd4db46c742c8c
James Kent <jameschristopherkent@gmail.com>
2016-12-31 10:20:26 +0000

    Problem 3 for go added

--- go/problem3.go
+++ go/problem3.go
@@ -8,7 +8,7 @@ import (
 func main(){
 
     var prime_number string
-    //var prime_factors []int
+    var prime_factors []int
 
     fmt.Printf("Please enter a number you want to prime factor: ")
     fmt.Scanln(&prime_number)
@@ -19,9 +19,32 @@ func main(){
     }
     original_prime := prime_number_num
 
-    a:=1
-    for a==1{
-        for 
+    //Loop indices...
+
+
+    for {
+        var i int
+        for  i=2; i<=5000000;i++{
+            if prime_number_num%i == 0{
+                fmt.Printf("Prime factor found: %d\n",i)
+                prime_number_num = prime_number_num/i;
+                prime_factors = append(prime_factors,i)
+                break
+            } else {
+                continue
+            }
+
+        }
+        var check_var int = 1
+
+        for num := range prime_factors {
+
+            check_var = check_var * prime_factors[num]
+        }
+        if check_var == original_prime{
+            fmt.Printf("Looks like we found all the prime factors! \n")
+            break
+        }
 
 
 
